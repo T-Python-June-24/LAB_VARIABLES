dd7232deca49aa9ffa1bfcffa702ff8ce1462f9e
raynasser <rnff-333@hotmail.com>
2024-06-06 13:49:13 +0300

    Ray - LAB#1

---
+++ DAY3_LAB1.py
@@ -0,0 +1,17 @@
+# Define a variable that has a value of your favourite football team.
+# Define a variable “x“ has an integer value and a variable “y” has a float value.
+# Assign One Value to Multiple Variables (choose random names & values).
+# Assign multiple Values to Multiple Variables (choose random names & values).
+
+
+team = "NOTHING:)"
+
+x:int = 91
+y:float = 6.69
+
+ov1 = ov2 = ov3 = 190895
+
+v1, v2, v3 = 33, "del", 2.24
+
+print(f'\n', team, '\n', x, '\n', y, '\n', ov1, '\n', ov2, '\n', ov3, '\n', v1, '\n', v2, '\n', v3)
+
