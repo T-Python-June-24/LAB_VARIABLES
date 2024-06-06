38f75073e9612d2877f29c34fe3a93ef1e85305a
AZ-007 <aboodi-75-@hotmail.com>
2024-06-06 15:00:04 +0300

    Arithmetic operations

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,32 @@
+
+print (5+3)
+
+print (5-2)
+
+print (5*4)
+
+print (5/2)
+
+print (5//2)
+
+print ("remainder",5 % 3)
+
+print (4**3) #exponentiation
+
+
+#precedence 
+''' 
+- parentheses ()
+- exponentiation
+- multiplication, division
+- addition , subtraction
+'''
+
+print (1+3 / 3*3)
+
+result = (5+3) / 3*3
+print(round (result, 4)) # round function is for approximation and I can set how many digits after fraction
+
+print( 5**(2*2))
+print (2**2**4)
+
