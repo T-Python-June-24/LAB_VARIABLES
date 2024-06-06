55783e0aa99eb6132368be7294f0dc1ec26b31b7
newta105 <119808370+newta105@users.noreply.github.com>
2024-06-06 14:13:48 +0300

    Noor lab_variable edit

--- labVariables.py
+++ labVariables.py
@@ -1,5 +1,9 @@
-my_fav_football_team = Hilal
+my_fav_football_team = "Hilal"
 x = 1
 y = 1.5
-one, two, three = 15
+one = two = three = 15
 four, five = 4, 5
+
+print(my_fav_football_team)
+print(one)
+print(four)
