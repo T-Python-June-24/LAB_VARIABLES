9b17f417a4b863d2936354d2528ea126fc36dc29
Abdullah Aldhamen <a.h.aldhamen@hotmail.com>
2024-06-06 14:04:10 +0300

    Lab is solved with printing values

---
+++ MyApp1.py
@@ -0,0 +1,12 @@
+MyFavTeam = "Barcelona"
+x = 10
+y = 9.9
+a = b = c = d = 5
+e,f,g,h = 1,[7,8,9],3.5,"Handball"
+
+print(MyFavTeam)
+print(x)
+print(y)
+print(a,b,c,d)
+print(e,f,g,h)
+print(x+y+a+b+c+d+e+g)
