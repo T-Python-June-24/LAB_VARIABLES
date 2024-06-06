29f9c9897aafc281802ab8d9c31b52504ba8d893
bushraali98 <bm-bisho-90210@hotmail.com>
2024-06-06 14:29:29 +0300

    lab solution done

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ x = 24
 y = 10.5
 
 # Third question answer
-age1, age2, age3 = 23
+age1 = age2 = age3 = 23
 
 # Forth question answer 
 name, fav_color, fav_number = "Boshra", "Blue", 1
+
+# print (age1, age2, age3)
