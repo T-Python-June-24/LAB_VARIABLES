baa863d68ac46692ebcde209f8e1c2482a59c76f
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-06-06 13:41:36 +0300

    "Completed the lab"

---
+++ main.py
@@ -0,0 +1,7 @@
+football_team:str = "I don't watch football"
+x:int = 10
+y:float = 10.0
+# Multiple variables using one value
+a = b = c = 5
+# Multiple variables using multiple values
+d, e, f = 1, 2, 3
