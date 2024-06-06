fbdf6ca2dc03180a2555be8f055cfbcaa98eb325
bushraali98 <bm-bisho-90210@hotmail.com>
2024-06-06 14:24:24 +0300

    lab solved

---
+++ main.py
@@ -0,0 +1,12 @@
+# First question answer
+fav_football_team = "Real Madrid"
+
+# Second question answer
+x = 24
+y = 10.5
+
+# Third question answer
+age1, age2, age3 = 23
+
+# Forth question answer 
+name, fav_color, fav_number = "Boshra", "Blue", 1
