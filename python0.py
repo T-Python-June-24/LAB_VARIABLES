05de6ab3da11911b08ffedda85195ff8c4f1941a
Ghofran <ghofranalsanosi@gmail.com>
2024-06-06 14:02:40 +0300

    solution LAB_VARIABLES

---
+++ python0.py
@@ -0,0 +1,8 @@
+MyFavouriteFootball= "Hilal"
+print(MyFavouriteFootball)
+x , y = 13 , 3.0
+print(x , y)
+num1=num2=num3=100
+print(num1,num2,num3)
+num1, num2 ,num3= 10,20,30
+print(num1 , num2 , num3)
